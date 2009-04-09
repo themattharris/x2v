e30fc9b99d721272c5f7584a8cab37107d8326d4
Ben Ward <ben@ben-ward.co.uk>
2009-04-09 00:43:18 -0700

    Added configurable CHARSET. Added licensing info to the README.

--- get-contact.php
+++ get-contact.php
@@ -10,25 +10,31 @@ version 1.0
 NOTES:
 This required PHP XSLT libraries installed
 
-
 --------------------------------------------- */
+
+// Include shared properties:
+require_once('configuration.php');
 
 // Check for URL or a referrer
 $uri='';
 if (isset($_GET['referer'])) {
-  if (getenv("HTTP_REFERER") != ''){ $uri = getenv("HTTP_REFERER"); }
+  if (getenv("HTTP_REFERER") != '') { 
+      $uri = getenv("HTTP_REFERER"); 
+  }
 }
 
 // if a url is specified then use that
 if (isset($_GET['uri'])){
-  if ($_GET['uri'] != ''){ $uri = $_GET['uri']; }
+  if ($_GET['uri'] != '') {
+      $uri = $_GET['uri'];
+  }
 }
 
 
 if ((substr($uri, 0,7) == "http://") || (substr($uri, 0,8) == "https://")) {
-set_time_limit(90);
+    
+    set_time_limit(90);
 
-	$outputEncoding = 'utf-8';
 	// check to see if a filename is specified
 	if (isSet($_GET['filename'])){
 	  if ($_GET['filename'] != ''){ $logfile = $_GET['filename']; }
@@ -36,7 +42,11 @@ set_time_limit(90);
 
 	// explode on the '#' to separate the anchor link from the page
 	$temp = explode("#",$uri);
-    if(isSet($temp[1])){ $anchor = $temp[1]; } else { $anchor = '';}
+    if(isSet($temp[1])){ 
+        $anchor = $temp[1];
+    } else {
+        $anchor = '';
+    }
     $temp = $temp[0];
     
 	$c = curl_init();
@@ -44,7 +54,7 @@ set_time_limit(90);
 	curl_setopt($c, CURLOPT_URL, str_replace('&amp;','&',$uri));
 	curl_setopt($c, CURLOPT_CONNECTTIMEOUT, 2);
 	curl_setopt($c, CURLOPT_TIMEOUT, 4);
-	curl_setopt($c, CURLOPT_USERAGENT, "curl from http://suda.co.uk/projects/microformats/ ");
+	curl_setopt($c, CURLOPT_USERAGENT, X2V_USER_AGENT);
 	curl_setopt($c, CURLOPT_FOLLOWLOCATION,1);
 	$xml_string = curl_exec($c);
 	$info = curl_getinfo($c);
@@ -63,16 +73,27 @@ set_time_limit(90);
 	
 	if (isset($_GET['type'])){
 		switch(strtolower($_GET['type'])){
-		    case "rdf":     $extension .= "rdf"; $contentType = 'application/xml+rdf'; $filename = "/users/home/briansuda/web/public/projects/microformats/hcard/xhtml2vcardrdf.xsl"; break;
-		    default:    $filename = '/users/home/briansuda/web/public/projects/microformats/hcard/xhtml2vcard.xsl'; $contentType = "text/x-vcard"; $extension = 'vcf'; break;
+		    case "rdf":     
+		        $extension .= "rdf";
+		        $contentType = 'application/xml+rdf';
+		        $filename = X2V_ROOT_PATH . '/hcard/xhtml2vcardrdf.xsl';
+		        break;
+		    default:
+		        $filename = X2V_ROOT_PATH . '/hcard/xhtml2vcard.xsl';
+		        $contentType = "text/x-vcard";
+		        $extension = 'vcf';
+		        break;
 		}
-	} else { $filename = '/users/home/briansuda/web/public/projects/microformats/hcard/xhtml2vcard.xsl'; $contentType = "text/x-vcard"; $extension = 'vcf';}
+	} else { 
+	    $filename = X2V_ROOT_PATH . '/hcard/xhtml2vcard.xsl';
+	    $contentType = "text/x-vcard"; $extension = 'vcf';
+	}
 	
 	$doc->load($filename);
 	$xsl->importStyleSheet($doc);
 	$xsl->setParameter('', 'Source', $uri);
 	$xsl->setParameter('', 'Anchor', $anchor);
-	$xsl->setParameter('', 'Encoding', $outputEncoding);
+	$xsl->setParameter('', 'Encoding', X2V_CHARSET);
 
 	$doc->loadHTML($xml_string);
 	$doc->formatOutput = true;
@@ -85,7 +106,7 @@ set_time_limit(90);
 		header("Content-Disposition: attachment; filename=$logfile.$extension");
 	//	header("Content-Length: ".mb_strlen($Str)+1);
 		header("Connection: close");
-		header("Content-Type: $contentType; charset=$outputEncoding; name=$logfile.$extension");
+		header("Content-Type: $contentType; charset=".X2V_CHARSET."; name=$logfile.$extension");
 		echo $Str;
 		exit();
 	}
