eab0f4c5d9983436182425d6a1c9207669cae931
Matt <matt@themattharris.com>
2009-10-29 17:03:38 -0700

    Remove type in configuration.php

--- configuration.php
+++ configuration.php
@@ -2,6 +2,6 @@
     /* Define some shared configuration properties */
     define('X2V_USER_AGENT', 'Curl from ' + $_SERVER["SERVER_NAME"]);
     define('X2V_ROOT_PATH', realpath(dirname(__FILE__)));
-    define('X2V_CHARSET'), 'utf-8');
+    define('X2V_CHARSET', 'utf-8');
     
 ?>
